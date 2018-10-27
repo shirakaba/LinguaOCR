0bd8fffa08880c8bb91a9f8cbf81ab78272fa817
Jamie Birch work laptop <jbizzle5@gmail.com>
2018-10-27 13:51:23 +0100

    Best so far

--- app/main-view-model.ts
+++ app/main-view-model.ts
@@ -1,4 +1,14 @@
+/// <reference path="../node_modules/nativescript-http-server/typings/objc!GCDWebServer.d.ts" />
+
 import { Observable } from 'data/observable';
+
+// import { GCDWebServer } from 'nativescript-http-server/typings/objc!GCDWebServer.d.ts';
+// import { GCDWebServer } from 'nativescript-http-server';
+// import { GCDWebServerCopy } from 'nativescript-http-server';
+// import { GCDWebServerInstance } from 'nativescript-http-server';
+// const $GCDWebServer: GCDWebServer = require("GCDWebServer");
+
+import { HttpServer } from 'nativescript-http-server';
 
 export class HelloWorldModel extends Observable {
 
@@ -11,6 +21,23 @@ export class HelloWorldModel extends Observable {
         // Initialize default values.
         this._counter = 42;
         this.updateMessage();
+
+        // const GCDWebServer = require("GCDWebServer");
+        // const webServer = GCDWebServerInstance.alloc().init();
+        // const webServer = GCDWebServer.alloc().init();
+        // GCDWebServerInstance.addGETHandlerForBasePathDirectoryPathIndexFilenameCacheAgeAllowRangeRequests
+        // GCDWebServerInstance.start();
+
+        const httpServer: GCDWebServer = new HttpServer()._webServer;
+        // let localUrl = httpServer.serveWithHtml("<html><body><p>Hello World</p></body></html>");
+
+        // GCDWebServerInstance.addDefaultHandlerForMethodRequestClassAsyncProcessBlock("GET", GCDWebServerRequest, (request,completionBlock) => {
+        //     var response = GCDWebServerDataResponse.alloc().initWithHTML("some html string");
+        //     completionBlock(response);
+        // });
+        
+        
+        // webServer.start();
     }
 
     get message(): string {
